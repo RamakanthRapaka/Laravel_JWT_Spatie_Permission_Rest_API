65803291de932218b1727f7e4f0fe11f9385363f
Ramakanth8824 <ramakanth@credright.com>
2020-07-07 14:53:54 +0530

    ajax calls added

---
+++ public/assets/ajax/assignrole.js
@@ -0,0 +1,24 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$("#createpermission").click(function () {
+    $.ajax({
+        url: 'http://arc.test/api/v1/login',
+        method: 'POST',
+        data: {
+            email: $("#email").val(),
+            password: $("#password").val()
+        },
+        success: function (data) {
+            if (data.status_code == '200') {
+                localStorage.setItem("api_token", data.data.api_token);
+                window.location.replace('/home');
+            }
+            if (data.status_code == '422') {
+                alert('Validation Failed');
+            }
+        }
+    });
+});
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,35 @@ Route::get('/', function () {
 Route::get('/home', function () {
     return view('home');
 });
+
+Route::get('/users', function () {
+    return view('users');
+});
+
+Route::get('/createuser', function () {
+    return view('createuser');
+});
+
+Route::get('/roles', function () {
+    return view('roles');
+});
+
+Route::get('/createrole', function () {
+    return view('createrole');
+});
+
+Route::get('/assignrole', function () {
+    return view('assignrole');
+});
+
+Route::get('/permissions', function () {
+    return view('permissions');
+});
+
+Route::get('/createpermission', function () {
+    return view('createpermission');
+});
+
+Route::get('/assignpermission', function () {
+    return view('assignpermission');
+});
